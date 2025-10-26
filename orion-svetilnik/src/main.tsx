16d08e3ec70a6fc7cfb9bc463f7a67ced33c313f
SabaFTW <SabaFTW@users.noreply.github.com>
2025-10-26 23:17:11 +0100

    dasd

--- orion-svetilnik/src/main.tsx
+++ orion-svetilnik/src/main.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+// If App.tsx exists in src/, use:
+import App from './App'
+
+// Or, if App.tsx is in a different folder, update the path accordingly, e.g.:
+// import App from './components/App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
